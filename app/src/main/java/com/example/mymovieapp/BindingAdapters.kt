18525138f6ca041c091b95573b76a9f20425decf
Sanda Albrecht <sanda_albrecht@Deniss-MacBook-Pro.local>
2021-09-09 17:55:55 +0300

    Added loading and error images

--- app/src/main/java/com/example/mymovieapp/BindingAdapters.kt
+++ app/src/main/java/com/example/mymovieapp/BindingAdapters.kt
@@ -4,6 +4,7 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("posterUrl")
 fun bindImage(imgView: ImageView, posterPath: String?) {
@@ -11,6 +12,10 @@ fun bindImage(imgView: ImageView, posterPath: String?) {
         val posterUri = posterPath.toUri().buildUpon().scheme("https").build()
         Glide.with(imgView.context)
             .load(posterUri)
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_baseline_broken_image))
             .into(imgView)
     }
 }
