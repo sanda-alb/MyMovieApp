31a16b39a4a20abbf4ce06fe1b71a85c269dc276
Sanda Albrecht <sanda_albrecht@Deniss-MacBook-Pro.local>
2021-09-13 16:46:35 +0300

    DetailViewModel and detail layout

--- app/src/main/java/com/example/mymovieapp/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/mymovieapp/detail/DetailViewModel.kt
@@ -2,8 +2,18 @@ package com.example.mymovieapp.detail
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.mymovieapp.domain.MovieItem
 
 
-class DetailViewModel(@Suppress("UNUSED_PARAMETER")movieItem: MovieItem, app: Application) : AndroidViewModel(app) {
+class DetailViewModel( movieItem: MovieItem,
+                       app: Application) : AndroidViewModel(app) {
+    private val _selectedMovie = MutableLiveData<MovieItem>()
+    val selectedMovie: LiveData<MovieItem>
+        get() = _selectedMovie
+
+    init{
+        _selectedMovie.value = movieItem
+    }
 }
